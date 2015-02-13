638f0136561b892adb0227956740152e6166968f
Sasikala Ravichandran <passion.rails@gmail.com>
2015-02-14 05:04:00 +0530

    Substring

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,14 +1,13 @@
- def substrings(word,arr)
- 	result={}
+def substrings(word,arr)
+ 	result= Hash.new(0)
  	word_array = word.downcase.split
- 	puts word_array
  	arr.each do |wd|
  		word_array.each do |a|
  		     result[wd] ? result[wd] = result[wd] + 1 : result[wd] =  1 if a.include?(wd) 
         end
  	end
-     result
- end
+  result
+end
 
 dic = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit","low"]
 puts substrings("Howdy partner, sit down! How's it going?", dic).inspect
