b7e0509439f36bccfac2034b2a1a1fb34ae67dfc
Sasikala Ravichandran <passion.rails@gmail.com>
2015-02-14 04:53:02 +0530

    Stock Price

--- stock_price.rb
+++ stock_price.rb
@@ -15,29 +15,4 @@ end
 
 p stock([1,3,6,9,15,8,6,1,17]) 
  
-=begin
-def stock_picker(array)
-	brr = []
-	mp = 0
-	buy = 0
-	sell = 0
-	array.reverse!
-	array.each do |n|
-		array[array.index(n)..-1].each do |i|
-			 
-			if (n - i) > mp
-				mp = n -i 
-				buy = i
-				sell = n
-			end
-		end
-	end
-	array.reverse!	 
-	brr << array.index(buy)
-	brr << array.index(sell)
-	 
- p brr
-
-end
-stock_picker([17,3,6,9,15,8,6,1,10])
-=end
+ 
