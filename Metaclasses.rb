ed3faaca83d23d09e9d4f9c7a10bc55b0ebdf238
Sasikala Ravichandran <passion.rails@gmail.com>
2015-02-20 07:27:12 +0530

    Metaclasses

---
+++ Metaclasses.rb
@@ -0,0 +1,14 @@
+class A
+	class << self
+   class << self
+  	class << self
+      def self.hi
+         puts "hi"
+     end
+ end
+  	end
+end
+end
+  	 
+
+  	A.singleton_class.singleton_class.singleton_class.hi
